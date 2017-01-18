73ea1680296fef29b6a1d3c18c9cc01163a248b4
Unknown <kalmera74@gmail.com>
2017-01-18 18:32:06 +0300

    MAna Bar sistemi %70
    Çok şaşalı değil ama basit

--- Assets/Scripts/Powers/Mana System/ManaBar.cs
+++ Assets/Scripts/Powers/Mana System/ManaBar.cs
@@ -4,13 +4,12 @@ using System.Collections;
 public class ManaBar : MonoBehaviour
 {
 
-    public float BarPosX, BarPosY;
-    public Texture Fill;
     [Range(0.0f, 200.0f)]
-    public float MaxMana;
-    [Range(1.0f, 5.0f)]
+    public int MaxMana;
+    [Range(1.0f, 10.0f)]
     public float RechargeSpeed;
-    public float _p1, _p2;//Will be private
+    public int UseSpeed;
+    public float Player1Mana, Player2Mana;//Will be private
 
     // Use this for initialization
     void Start()
@@ -22,21 +21,49 @@ public class ManaBar : MonoBehaviour
     void Update()
     {
         Increase();
-
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Decrease();
     }
 
     void OnGUI()
     {
-        GUI.Box(new Rect(BarPosX, BarPosY, (Screen.width / 2) - 150, (Screen.width / 24)), Fill);
-        // GUI.Box(new Rect((Screen.width / 2), BarPosY, (Screen.width / 2)- 150, (Screen.width / 24)), "Player A");
-        GUI.Box(new Rect((Screen.width / 2) + 150, BarPosY, (Screen.width / 2) - 150, (Screen.width / 24)), Fill);
+        GUI.Box(new Rect(0, 0, (Screen.width / 2) - 150, (Screen.width / 24)),(int)Player1Mana + "/" + MaxMana);
+
+        GUI.Box(new Rect((Screen.width / 2) + 150, 0, (Screen.width / 2) - 150, (Screen.width / 24)), (int)Player2Mana + "/" + MaxMana);
 
     }
     public void Increase()
     {
         if (Network.isServer)
-            _p1++;
+        {
+            if (Player1Mana >= MaxMana)
+                Player1Mana = MaxMana;
+            else
+                 Player1Mana += RechargeSpeed * Time.deltaTime;
+        }
         else
-            _p2++;
+        {
+            if (Player2Mana >= MaxMana)
+                Player2Mana = MaxMana;
+            else
+                Player2Mana += RechargeSpeed * Time.deltaTime;
+        }
+    }
+    public void Decrease()
+    {
+        if (Network.isServer)
+        {
+            if (Player1Mana <= 0 || UseSpeed > Player1Mana)
+                Player1Mana = 0;
+            else
+                Player1Mana -= UseSpeed;
+        }
+        else
+        {
+            if (Player2Mana <= 0||UseSpeed>Player2Mana)
+                Player2Mana = 0;
+            else
+                Player2Mana -= UseSpeed;
+        }
     }
 }
